628881cfe8028230351c85af9f8bba3904f6a58d
Miguel lopes <mzramnadev@gmail.com>
2020-05-05 22:58:00 -0300

    versão 1.5 pronta tts e stt para pt-br habilitadas

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess as s
 
 from gtts import gTTS
 from pydub import AudioSegment
-
+import speech_recognition as sr
 
 def tts(texto, save="./teste.mp3", slow=False, lang="pt-br"):  # ok
     voice = gTTS(text=texto, lang=lang, slow=slow)
@@ -28,7 +28,6 @@ def tts_string(texto, output="./output.mp3", tmp="./tmp.mp3", delay_virgula=200,
     tmp1 = 0
     audio = AudioSegment.silent(duration=0)
     for i in range(0, len(texto)):
-        print(texto[tmp1:i])
         if texto[i] == ",":
             tts(texto[tmp1:i], save=tmp, lang=lang, slow=slow)
             audio += AudioSegment.from_mp3(tmp)
@@ -47,8 +46,22 @@ def tts_string(texto, output="./output.mp3", tmp="./tmp.mp3", delay_virgula=200,
     os.remove(tmp)
     audio.export(output)
 
-
-tts_string(
-    "esse é um teste da nova função,se tudo estiver funcionando corretamente,haverão duas pequenas pausas.seguidas por uma longa pausa",
-    output="teste.mp3")
-s.call(["C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", './teste.mp3'])
+def stt(lang="pt-br"):
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("diga algo")
+        audio=r.listen(source)
+        try:
+            captado=r.recognize_google(audio,language=lang)
+            return(captado)
+        except Exception as e:
+            print(e)
+            return stt(lang)
+# tts_string(
+#     "esse é um teste da nova função,se tudo estiver funcionando corretamente,haverão duas pequenas pausas.seguidas por uma longa pausa",
+#     output="teste.mp3")
+capturado=stt()
+print(capturado)
+tts_string(capturado,output="teste_de_stt_to_tts.mp3")
+s.call(["C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", './teste_de_stt_to_tts.mp3'])
