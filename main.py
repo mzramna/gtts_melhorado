7221b6cff9f6de06da0289a90809443d05744019
Miguel lopes <mzramnadev@gmail.com>
2020-09-22 11:31:05 -0300

    tentativa de implementar com pocketsphinx

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import subprocess as s
 from gtts import gTTS
 from pydub import AudioSegment
 import speech_recognition as sr
+import moviepy.editor as mp
+from pocketsphinx import LiveSpeech, get_model_path, get_data_path,AudioFile
 
 def tts(texto, save="./teste.mp3", slow=False, lang="pt-br"):  # ok
     voice = gTTS(text=texto, lang=lang, slow=slow)
@@ -59,22 +61,31 @@ def stt(lang="pt-br"):
             print(e)
             return stt(lang)
         
-def stt_file(file_mp3,lang="pt-br"):
-    r=sr.Recognizer()
-    with  sr.AudioFile(file_mp3) as source:
-        r.adjust_for_ambient_noise(source)
-        print("diga algo")
-        audio=r.listen(source)
-        try:
-            captado=r.recognize_google(audio,language=lang)
-            return(captado)
-        except Exception as e:
-            print(e)
-            return stt(lang)
+def stt_file(file,lang="pt-br"):
+    model_path = get_model_path()
+    data_path = get_data_path()
+    clip = mp.VideoFileClip(file)
+    clip.audio.write_audiofile("./audio.wav")
+    config = {
+        'verbose': False,
+        'audio_file': os.path.join(data_path,"./audio.wav"),
+        'buffer_size': 2048,
+        'no_search': False,
+        'full_utt': False,
+        'hmm': os.path.join(model_path, lang),
+        'lm': os.path.join(model_path, lang+'.lm.bin'),
+        'dict': os.path.join(model_path, 'cmudict-'+lang+'.dict')
+    }
+
+
+    for phrase in AudioFile(**config):
+        print(phrase)
+
+
 # tts_string(
 #     "esse é um teste da nova função,se tudo estiver funcionando corretamente,haverão duas pequenas pausas.seguidas por uma longa pausa",
 #     output="teste.mp3")
-capturado=stt()
+capturado=stt_file(file="%UserProfile%\\Pictures\\WhatsApp Video 2020-09-22 at 08.42.54.mp4")
 print(capturado)
-tts_string(capturado,output="teste_de_stt_to_tts.mp3")
-s.call(["C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", './teste_de_stt_to_tts.mp3'])
+#tts_string(capturado,output="teste_de_stt_to_tts.mp3")
+#s.call(["C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", './teste_de_stt_to_tts.mp3'])
